46b8ca8e1c158b38d608668c68793f7d433bd90d
Shruthi <shgowda@expedia.com>
2016-07-27 12:43:26 -0700

    added files to redBlueBomb sample project

--- RedBlueBomb/ViewController.swift
+++ RedBlueBomb/ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redButton: UIButton!
+    
+    @IBOutlet weak var blueButton: UIButton!
+    
+    @IBOutlet weak var blueImage: UIImageView!
+    
+    @IBOutlet weak var redImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickRedButton(sender: AnyObject) {
+        redImage.hidden = true
+        blueImage.hidden = false
+    }
+    
+    
+    @IBAction func clickBlueButton(sender: AnyObject) {
+        blueImage.hidden = true
+        redImage.hidden = false
+    }
 
 }
 
